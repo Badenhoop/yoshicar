12c723b3c17dcbfb3fcf589d370b3f4c95e1910a
Hoop77 <p.badenhoop@gmx.de>
2019-05-03 22:39:03 +0200

    rm cmake-build-debug

--- src/ros_ws/src/remote_control/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/philipp/slam-car/src/ros_ws/src/remote_control
-# Build directory: /home/philipp/slam-car/src/ros_ws/src/remote_control/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
